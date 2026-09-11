1707f1278499b76c645e2bf39287f0233f4c5575
julie016 <lecheta.julie@escola.pr.gov.br>
2026-09-10 21:34:01 -0300

    10/09

--- main.js
+++ main.js
@@ -68,5 +68,5 @@ function classificaSenha(tamanhoAlfabeto) {
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = "um computador pode levar até "+ Math.floor (2**entropia/(100e6*60*60*24))+" dias para descobrir essa senha.";
+    valorEntropia.textContent = "um computador pode levar ate "+ Math.floor (2**entropia/(100e6*60*60*24))+" dias para descobrir essa senha.";
 }
